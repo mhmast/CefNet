37e03bbc4df8470e86eab894e6a829298dc883ce
vmas <vladsv88@gmail.com>
2021-10-20 13:40:03 +0300

    Fix ArgumentException when CefTime is empty.

--- CefNet/CefTypes/CefTime.cs
+++ CefNet/CefTypes/CefTime.cs
@@ -24,7 +24,12 @@ namespace CefNet
 
 		public DateTime ToDateTime()
 		{
-			return new DateTime(Year, Month, DayOfMonth, Hour, Minute, Second, DateTimeKind.Utc);
+			if (Year >= 1 && Year <= 9999 && Month >= 1 && Month <= 12 && DayOfMonth >= 1
+				&& Hour >= 0 && Hour < 24 && Minute >= 0 && Minute < 60 && Second >= 0 && Second < 60)
+			{
+				return new DateTime(Year, Month, DayOfMonth, Hour, Minute, Second, DateTimeKind.Utc);
+			}
+			return default;
 		}
 	}
 }
