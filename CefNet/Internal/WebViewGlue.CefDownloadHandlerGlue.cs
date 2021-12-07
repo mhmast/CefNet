fdc68b21202a3a56f993fd588737340cacaa0d47
vmas <vladsv88@gmail.com>
2021-12-07 22:56:35 +0300

    Add docs.

--- CefNet/Internal/WebViewGlue.CefDownloadHandlerGlue.cs
+++ CefNet/Internal/WebViewGlue.CefDownloadHandlerGlue.cs
@@ -23,6 +23,17 @@ namespace CefNet.Internal
 		[MethodImpl(MethodImplOptions.ForwardRef)]
 		internal extern bool AvoidOnBeforeDownload();
 
+		/// <summary>
+		/// Called before a download begins.
+		/// </summary>
+		/// <param name="browser">The browser.</param>
+		/// <param name="suggestedName">The suggested name for the download file.</param>
+		/// <param name="downloadItem">Do not keep a reference to <paramref name="downloadItem"/> outside of this function.</param>
+		/// <param name="callback">
+		/// Execute <paramref name="callback"/> either asynchronously or in this function
+		/// to continue the download if desired.
+		/// </param>
+		/// <remarks>By default the download will be canceled.</remarks>
 		internal protected virtual void OnBeforeDownload(CefBrowser browser, CefDownloadItem downloadItem, string suggestedName, CefBeforeDownloadCallback callback)
 		{
 
@@ -31,6 +42,17 @@ namespace CefNet.Internal
 		[MethodImpl(MethodImplOptions.ForwardRef)]
 		internal extern bool AvoidOnDownloadUpdated();
 
+		/// <summary>
+		/// Called when a download&apos;s status or progress information has been updated.
+		/// </summary>
+		/// <param name="downloadItem">
+		/// Do not keep a reference to <paramref name="downloadItem"/> outside of this function.
+		/// </param>
+		/// <param name="callback">
+		/// Execute <paramref name="callback"/> either asynchronously or in this function to cancel the
+		/// download if desired.
+		/// </param>
+		/// <remarks>This may be called multiple times before and after <see cref="OnBeforeDownload"/>.</remarks>
 		internal protected virtual void OnDownloadUpdated(CefBrowser browser, CefDownloadItem downloadItem, CefDownloadItemCallback callback)
 		{
 
